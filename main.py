de7f54c6e4730339a6276c1658e92d0c2a4111f4
Yeongho Kim <yeonghoey@gmail.com>
2016-05-30 17:42:35 +0900

    Implement main.py

--- main.py
+++ main.py
@@ -2,16 +2,38 @@ from __future__ import print_function
 
 import sys
 import time
-
 from pprint import pprint
 
 import requests
 
-
 GITHUB = 'https://api.github.com'
 
-def gh(uri):
+def G(uri):
     return '{0}/{1}'.format(GITHUB, uri)
+
+def main(getter):
+    url = G('users?per_page=100')
+    while True:
+        us, url = users(getter, url)
+        for u in us:
+            for r in starred(getter, u):
+                print('%s\t%s' % (u, r))
+
+def users(getter, url):
+    r = getter(url)
+    names = [ u['login'] for u in r.json() ]
+    nexturl = r.links.get('next', {}).get('url', '')
+    return (names, nexturl)
+
+def starred(getter, user):
+    r = getter(G('users/%s/starred?sort=created&per_page=100' % user))
+    return [repo['full_name'] for repo in r.json()]
+
+def rate_limit(getter):
+    r = getter(G('rate_limit'))
+    core = r.json()['resources']['core']
+    return (core['limit'], core['remaining'], core['reset'])
+
 
 def new_getter(token = None):
     headers = {}
@@ -44,30 +66,10 @@ def wait(reset):
         print('wait for reset: %s s remaiting' % secs, file=sys.stderr)
         time.sleep(1)
 
-def main(getter):
-    url = gh('users?per_page=100')
-    while True:
-        us, url = users(getter, url)
-        for u in us:
-            for r in starred(getter, u):
-                print('%s,%s' % (u, r))
-
-def users(getter, url):
-    r = getter(url)
-    names = [ u['login'] for u in r.json() ]
-    nexturl = r.links.get('next', {}).get('url', '')
-    return (names, nexturl)
-
-def starred(getter, user):
-    r = getter(gh('users/%s/starred?sort=created&per_page=100' % user))
-    return [repo['full_name'] for repo in r.json()]
-
-def rate_limit(getter):
-    r = getter(gh('rate_limit'))
-    core = r.json()['resources']['core']
-    return (core['limit'], core['remaining'], core['reset'])
 
 if __name__ == '__main__':
-    token = raw_input('Personal access token: ')
-    print('')
-    main(new_getter(token))
+    token = ''
+    if len(sys.argv) == 2:
+        token = sys.argv[1]
+    getter = new_getter(token)
+    main(getter)
